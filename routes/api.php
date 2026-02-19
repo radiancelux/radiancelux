102f3400513cd9e64e470ebf3b2c6a535e1d7a68
Mateus Galasso <mateusgalasso@gmail.com>
2026-02-18 21:47:25 -0300

    feat: add health check endpoint for Coolify

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/health', fn () => response()->json(['status' => 'ok']));
+
 Route::post('/contact', [ContactController::class, 'store']);
